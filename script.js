b944d76c1065dd6350bc2f2ee8ac9b71f12d5e0e
vmayorov <vmayorov@nxsystemsinc.com>
2018-08-07 20:09:59 +0300

    updated

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
 (function(window) {
     var testFrame = document.getElementById('testFrame');
 
+    function array(elm) {
+        if (Array.isArray(elm)) {
+            return elm;
+        } else {
+            return [elm];
+        }
+    }
+
+    function tag(name, attrs, nodes = []) {
+        var elm = document.createElement(name);
+        for (var prop in attrs) {
+            elm.setAttribute(prop, attrs[prop]);
+        }
+
+        array(nodes).forEach(function(node) {
+            elm.appendChild(node);
+        });
+
+        return elm;
+    }
+
+    function text(str) {
+        return document.createTextNode(str);
+    }
+
+    function div(attrs, nodes = []) {
+        return tag('div', attrs, nodes);
+    }
+
+    function replaceAllChildren(parent, nodes) {
+        // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+        var fc = parent.firstChild;
+
+        while( fc ) {
+            parent.removeChild( fc );
+            fc = parent.firstChild;
+        }
+
+        array(nodes).forEach(function(item) {
+            parent.appendChild(item);
+        });
+
+        return parent;
+    }
+
+    var testDb = {
+        words1: {
+            "count": {
+                min: 3,
+                max: 255
+            },
+            dictionary: ['words1.txt', 'words2.txt']
+        }
+    };
+
     function generateWords1(count) {
         // http://dict.ruslang.ru/
         fetch('words2.txt')
             .then(function(resp) {
                 return resp.text();
             })
-            .then(function(text) {
-                var arr = text.split("\n");
+            .then(function(str) {
+                var arr = str.split("\n");
                 var words = _.chain(arr)
                     .map(function(val) {
                         return val.trim();
@@ -16,11 +71,11 @@
                     .shuffle()
                     .take(count)
                     .value();
-                testFrame.innerHTML = words.join(', ').trim();
+                var sample = words.join(', ').trim();
+                replaceAllChildren(testFrame, div({'class': 'text-test', 'data-sample': sample}, text(sample)));
             });
     }
 
-    generateWords1(25);
-
+    generateWords1(5);
 
 })(window);
